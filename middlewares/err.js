32391ea63d5542e65fd93e6158bd742f01fe1580
Suvadip Paul <p.suvadip@gmail.com>
2020-08-08 20:06:51 +0530

    eslint error ignored

--- middlewares/err.js
+++ middlewares/err.js
@@ -1,5 +1,6 @@
 // define custom error-handling middleware function
 
+// eslint-disable-next-line no-unused-vars
 function errhandler(err, req, res, next) {
     return res.status(500).send(err.message);
 }
